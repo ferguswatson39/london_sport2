ed339f3ea4d901b4997353ff6c1af657105a75df
jn25200 <ferg.watson23@gmail.com>
2026-06-27 13:01:37 +0100

    bug fixes in gower distance and implemented umap with gower distance param

--- src/stream_3/gower_distance.py
+++ src/stream_3/gower_distance.py
@@ -2,6 +2,9 @@ import gower
 import pandas as pd
 import numpy as np
 
-def get_gower(categorical_cols : list[bool], df : pd.DataFrame) -> np.array:
-    gower_matrix = gower.gower_matrix(df, cat_features = categorical_cols)
+def get_gower(categorical_cols : list[str], df : pd.DataFrame) -> np.array:
+    cat_bool_mask = np.array([True if c in categorical_cols else False for c in df.columns])
+    gower_matrix = gower.gower_matrix(df, cat_features = cat_bool_mask)
+    for idx, value in enumerate(df.columns):
+        print(f'{idx} | {cat_bool_mask[idx]} | {value}')
     return gower_matrix
