c9a693ca5cbc7b6167599a5738f279dbc7b62c26
joshdfeather <joshdfeather@gmail.com>
2026-07-06 16:35:20 +0100

    KAN-50: Update prepare borough to be reusable

--- src/stream_1/prepare_boroughs.py
+++ src/stream_1/prepare_boroughs.py
@@ -6,29 +6,33 @@ ROOT = Path(__file__).resolve().parent.parent.parent
 sys.path.append(str(ROOT))
 from src.loading_data.load_data import get_borough_data
 
-df = get_borough_data()
-boroughs = df.groupby(['LA_2023', 'LA_Name', 'year'])['MEMS7_ALL'].mean()
-boroughs = boroughs.reset_index()
-boroughs['LA_Name'] = boroughs['LA_Name'].replace({'Kingston upon Thames': 'Kingston', 'Richmond upon Thames': 'Richmond'})
-boroughs = boroughs[boroughs['LA_2023'] != 59.0] # Removing City of London (COVID Outliers)
+def prepare_borough_data():
+    borough_df = get_borough_data()
+    borough_df = borough_df.groupby(['LA_2023', 'LA_Name', 'year'])['MEMS7_ALL'].mean()
+    borough_df = borough_df.reset_index()
+    borough_df['LA_Name'] = borough_df['LA_Name'].replace({'Kingston upon Thames': 'Kingston', 'Richmond upon Thames': 'Richmond'})
+    borough_df = borough_df[borough_df['LA_2023'] != 59.0] # Removing City of London (COVID Outliers)
+    return borough_df
 
-g = sns.relplot(kind = 'scatter', 
-            data = boroughs, 
-            x = 'year', 
-            y = 'MEMS7_ALL', 
-            col = 'LA_Name', 
-            col_wrap = 8,
-            height = 2, 
-            aspect = 1.4,
-            s = 50,
-            palette = 'RdYlGn',
-            hue = 'MEMS7_ALL',
-            legend = False,
-            edgecolor = 'grey', 
-            linewidth = 1)
-
-g.set_titles('{col_name}', weight = 'bold')
-g.set(xlabel = 'Year', ylabel = 'Avg Sport Participation')
-g.set_xticklabels([])
-g.set_yticklabels([])
-plt.show()
+# Initial Visualisation
+if __name__ == "__main__":
+    borough_df = prepare_borough_data()
+    g = sns.relplot(kind = 'scatter', 
+                data = borough_df, 
+                x = 'year', 
+                y = 'MEMS7_ALL', 
+                col = 'LA_Name', 
+                col_wrap = 8,
+                height = 2, 
+                aspect = 1.4,
+                s = 50,
+                palette = 'RdYlGn',
+                hue = 'MEMS7_ALL',
+                legend = False,
+                edgecolor = 'grey', 
+                linewidth = 1)
+    g.set_titles('{col_name}', weight = 'bold')
+    g.set(xlabel = 'Year', ylabel = 'Avg Sport Participation')
+    g.set_xticklabels([])
+    g.set_yticklabels([])
+    plt.show()
