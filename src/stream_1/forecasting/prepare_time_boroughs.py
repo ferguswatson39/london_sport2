e44f481c9d1ad01c192dc2057d0ceca0c59b4b28
everettmakes <jeverer@gmail.com>
2026-07-16 11:55:37 +0100

    prepare monthly, quarterly, yearly borough data for forecasting

---
+++ src/stream_1/forecasting/prepare_time_boroughs.py
@@ -0,0 +1,91 @@
+from pathlib import Path
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import sys
+ROOT = Path(__file__).resolve().parent.parent.parent.parent
+sys.path.append(str(ROOT))
+from src.loading_data.load_data import get_geographic_data
+from src.loading_data.data_catalogue import DataCatalogue
+
+def prepare_monthly_boroughs():
+    print("PREPARING MONTHLY DATA...")
+    dc = DataCatalogue()
+    df_path = Path(ROOT / 'exploration' / 'data' / 'master_data' / '2016_to_2023_full_preprocessed_data_set.csv.gz')
+    raw = pd.read_csv(df_path)
+    raw = raw.copy()
+
+    raw['month'] = ((raw['month'] - 3) % 12) + 1
+    raw['year'] = raw['year'].str.split('/').str[1].astype(int) + 2000
+    raw.loc[raw['month'].isin([11,12]), 'year'] = raw['year'] - 1  
+
+    raw['LA_Name'] = raw['LA_2023'].map(dc.get_data_dict()['LA_2023']['value_labels'])
+
+    borough_df = raw.groupby(['LA_2023', 'LA_Name', 'year', 'month']).agg(
+        MEMS7_ALL=('MEMS7_ALL', 'mean'),
+        IMD_mean=('IMD10', 'mean'),
+        LondInOut=('LondInOut', 'first')
+    ).reset_index().sort_values(['LA_2023', 'year', 'month']).reset_index(drop=True)
+    borough_df['LA_Name'] = borough_df['LA_Name'].replace({
+        'Kingston upon Thames': 'Kingston',
+        'Richmond upon Thames': 'Richmond'
+    })
+    borough_df['LondInOut'] = borough_df['LondInOut'].map({1.0: 'Outer', 2.0: 'Inner'})
+    borough_df = borough_df[borough_df['LA_2023'] != 59.0]
+    borough_df.sort_values(['LA_2023', 'year', 'month'], inplace=True)
+    print("COMPLETE")
+    return borough_df
+
+def prepare_quarterly_boroughs():
+    print("PREPARING QUARTERLY DATA...")
+    dc = DataCatalogue()
+    df_path = Path(ROOT / 'exploration' / 'data' / 'master_data' / '2016_to_2023_full_preprocessed_data_set.csv.gz')
+    raw = pd.read_csv(df_path)
+    raw = raw.copy()
+
+    raw['month'] = ((raw['month'] - 3) % 12) + 1
+    raw['year'] = raw['year'].str.split('/').str[1].astype(int) + 2000
+    raw.loc[raw['month'].isin([11,12]), 'year'] = raw['year'] - 1  
+
+    raw['quarter'] = pd.cut(raw['month'], bins=[0,3,6,9,12], labels=[1,2,3,4])
+    raw['LA_Name'] = raw['LA_2023'].map(dc.get_data_dict()['LA_2023']['value_labels'])
+
+    borough_df = raw.groupby(['LA_2023', 'LA_Name', 'year', 'quarter']).agg(
+        MEMS7_ALL=('MEMS7_ALL', 'mean'),
+        IMD_mean=('IMD10', 'mean'),
+        LondInOut=('LondInOut', 'first')
+    ).reset_index().sort_values(['LA_2023', 'year', 'quarter']).reset_index(drop=True)
+    borough_df['LA_Name'] = borough_df['LA_Name'].replace({
+        'Kingston upon Thames': 'Kingston',
+        'Richmond upon Thames': 'Richmond'
+    })
+    borough_df['LondInOut'] = borough_df['LondInOut'].map({1.0: 'Outer', 2.0: 'Inner'})
+    borough_df = borough_df[borough_df['LA_2023'] != 59.0]
+    borough_df.sort_values(['LA_2023', 'year', 'quarter'], inplace=True)
+    print("COMPLETE")
+    return borough_df
+
+def prepare_yearly_boroughs():
+    print("PREPARING YEARLY DATA...")
+    dc = DataCatalogue()
+    df_path = Path(ROOT / 'exploration' / 'data' / 'master_data' / '2016_to_2023_full_preprocessed_data_set.csv.gz')
+    raw = pd.read_csv(df_path)
+    raw = raw.copy()
+
+    raw['year'] = raw['year'].str.split('/').str[1].astype(int) + 2000
+    raw['LA_Name'] = raw['LA_2023'].map(dc.get_data_dict()['LA_2023']['value_labels'])
+
+    borough_df = raw.groupby(['LA_2023', 'LA_Name', 'year']).agg(
+        MEMS7_ALL=('MEMS7_ALL', 'mean'),
+        IMD_mean=('IMD10', 'mean'),
+        LondInOut=('LondInOut', 'first')
+    ).reset_index().sort_values(['LA_2023', 'year']).reset_index(drop=True)
+    borough_df['LA_Name'] = borough_df['LA_Name'].replace({
+        'Kingston upon Thames': 'Kingston',
+        'Richmond upon Thames': 'Richmond'
+    })
+    borough_df['LondInOut'] = borough_df['LondInOut'].map({1.0: 'Outer', 2.0: 'Inner'})
+    borough_df = borough_df[borough_df['LA_2023'] != 59.0]
+    borough_df.sort_values(['LA_2023', 'year'], inplace=True)
+    print("COMPLETE")
+    return borough_df
