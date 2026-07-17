4e3dab88283318116e313eaddc2ba9e757cdd0c5
everettmakes <jeverer@gmail.com>
2026-07-17 11:02:23 +0100

    Included covid correction to prepare_time_boroughs.py functions

--- src/stream_1/forecasting/prepare_time_boroughs.py
+++ src/stream_1/forecasting/prepare_time_boroughs.py
@@ -3,12 +3,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import sys
+
+from stream_1.covid_analysis import covid_adjustment
 ROOT = Path(__file__).resolve().parent.parent.parent.parent
 sys.path.append(str(ROOT))
 from src.loading_data.load_data import get_geographic_data
 from src.loading_data.data_catalogue import DataCatalogue
 
-def prepare_monthly_boroughs():
+def prepare_monthly_boroughs(ADJUST_COVID = False):
     print("PREPARING MONTHLY DATA...")
     dc = DataCatalogue()
     df_path = Path(ROOT / 'exploration' / 'data' / 'master_data' / '2016_to_2023_full_preprocessed_data_set.csv.gz')
@@ -20,6 +22,8 @@ def prepare_monthly_boroughs():
     raw.loc[raw['month'].isin([11,12]), 'year'] = raw['year'] - 1  
 
     raw['LA_Name'] = raw['LA_2023'].map(dc.get_data_dict()['LA_2023']['value_labels'])
+
+    raw = covid_adjustment(raw)
 
     borough_df = raw.groupby(['LA_2023', 'LA_Name', 'year', 'month']).agg(
         MEMS7_ALL=('MEMS7_ALL', 'mean'),
@@ -36,7 +40,7 @@ def prepare_monthly_boroughs():
     print("COMPLETE")
     return borough_df
 
-def prepare_quarterly_boroughs():
+def prepare_quarterly_boroughs(ADJUST_COVID = False):
     print("PREPARING QUARTERLY DATA...")
     dc = DataCatalogue()
     df_path = Path(ROOT / 'exploration' / 'data' / 'master_data' / '2016_to_2023_full_preprocessed_data_set.csv.gz')
@@ -49,6 +53,8 @@ def prepare_quarterly_boroughs():
 
     raw['quarter'] = pd.cut(raw['month'], bins=[0,3,6,9,12], labels=[1,2,3,4])
     raw['LA_Name'] = raw['LA_2023'].map(dc.get_data_dict()['LA_2023']['value_labels'])
+
+    raw = covid_adjustment(raw)
 
     borough_df = raw.groupby(['LA_2023', 'LA_Name', 'year', 'quarter']).agg(
         MEMS7_ALL=('MEMS7_ALL', 'mean'),
@@ -65,15 +71,20 @@ def prepare_quarterly_boroughs():
     print("COMPLETE")
     return borough_df
 
-def prepare_yearly_boroughs():
+def prepare_yearly_boroughs(ADJUST_COVId = False):
     print("PREPARING YEARLY DATA...")
     dc = DataCatalogue()
     df_path = Path(ROOT / 'exploration' / 'data' / 'master_data' / '2016_to_2023_full_preprocessed_data_set.csv.gz')
     raw = pd.read_csv(df_path)
     raw = raw.copy()
 
+    raw['month'] = ((raw['month'] - 3) % 12) + 1
     raw['year'] = raw['year'].str.split('/').str[1].astype(int) + 2000
+    raw.loc[raw['month'].isin([11,12]), 'year'] = raw['year'] - 1  
+
     raw['LA_Name'] = raw['LA_2023'].map(dc.get_data_dict()['LA_2023']['value_labels'])
+
+    raw = covid_adjustment(raw)
 
     borough_df = raw.groupby(['LA_2023', 'LA_Name', 'year']).agg(
         MEMS7_ALL=('MEMS7_ALL', 'mean'),
