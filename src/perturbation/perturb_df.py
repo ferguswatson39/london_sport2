812d20f01dac11e7925e3a1ca28075e903724096
jn25200 <ferg.watson23@gmail.com>
2026-07-19 14:47:20 +0100

    added comments to perturbation function

--- src/perturbation/perturb_df.py
+++ src/perturbation/perturb_df.py
@@ -2,6 +2,11 @@ import pandas as pd
 import numpy as np
 
 def perturb(df : pd.DataFrame, perturb_col : str) -> pd.DataFrame:
+    """ 
+    Perturbation function
+    Takes perturb_col as argument and creates new col with perturbed inputs
+    For obs which are already at the max, replaces these obs with NAN as to prevent impossible responses
+    """
     df = df.copy()
     max_val = df[perturb_col].max()
     p_name = f'PERTURBED_{perturb_col}'
