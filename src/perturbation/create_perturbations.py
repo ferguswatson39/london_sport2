550e45bf37fc830836ac4bfae1426565bb1950b4
jn25200 <ferg.watson23@gmail.com>
2026-07-20 19:23:00 +0100

    debugged create_perturbations py and added scaler object

--- src/perturbation/create_perturbations.py
+++ src/perturbation/create_perturbations.py
@@ -5,8 +5,9 @@ from pathlib import Path
 ROOT = Path(__file__).resolve().parent.parent.parent
 sys.path.append(str(ROOT))
 from src.perturbation.perturb_df import perturb
+from sklearn.preprocessing import StandardScaler
 
-def run_perturbation(df : pd.DataFrame, to_perturb : list, model : object):
+def run_perturbation(df : pd.DataFrame, to_perturb : list, model : object, scaler : object):
     """
     Run perturbations function.
     Requires model object to have get_proba() method -- Ensure that models used in perturbation have .get_proba() method
@@ -20,19 +21,21 @@ def run_perturbation(df : pd.DataFrame, to_perturb : list, model : object):
         new = df.copy()
         old_var_name = f'PREDS_{var}'
         new_var_name = f'PERTURBED_PREDS_{var}'
+        difference = f'DIFFERENCE_{var}'
 
         perturbed_vals = perturb(new, var)
         # Use ~ to invert condition thus returns True for int values and False for NaN
         #https://jakevdp.github.io/PythonDataScienceHandbook/02.06-boolean-arrays-and-masks.html
         nan_mask = ~np.isnan(perturbed_vals)
-        
+        X_scaled = scaler.transform(new[nan_mask])
         ## Generate the preds for non perturbed subset
-        non_p_preds = model.get_proba(new[nan_mask].values)
+        non_p_preds = model.get_proba(X_scaled)
 
         new.loc[nan_mask, var] = perturbed_vals[nan_mask]
-
-        p_preds = model.predict_proba(new[nan_mask].values)
+        new_X_scaled = scaler.transform(new[nan_mask])
+        p_preds = model.get_proba(new_X_scaled)
         
-        output_df.loc[nan_mask, old_var_name] = non_p_preds[: , 1]
-        output_df.loc[nan_mask, new_var_name] = p_preds[: , 1]
+        output_df.loc[nan_mask, old_var_name] = non_p_preds
+        output_df.loc[nan_mask, new_var_name] = p_preds
+        output_df[difference] = (output_df[new_var_name] - output_df[old_var_name])
     return output_df
