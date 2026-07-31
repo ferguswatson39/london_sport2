fd71a05d9d624012e4d1c6dd0c6ac47f7b3c66a6
jn25200 <ferg.watson23@gmail.com>
2026-07-31 10:37:36 +0100

    finalised cluster label generation pipeline, using data_dict to load on continuous and categorical features

--- src/stream_3/create_clustering_labels.py
+++ src/stream_3/create_clustering_labels.py
@@ -9,12 +9,22 @@ from sklearn.preprocessing import OneHotEncoder
 from src.stream_3.gen_kmean_clus import GenerateKMEANS
 from src.stream_3.gen_kprototypes_clus import GenerateKprototypes
 from src.stream_3.gen_hdb_clus import GenerateHDBSCAN2
+from src.loading_data.data_catalogue import DataCatalogue
 
 
 if __name__ == '__main__':
+
     arr_path = ROOT / 'src' / 'stream_3' / 'embeddings' / # Enter umap emb .npy file
     save_path = ROOT / 'src' / 'stream_3' / 'cluster_labels' 
+    data_path = ROOT / 'data' / # Enter clustering dataset path
     arr = np.load(arr_path)
+    df = pd.read_csv(data_path)
+
+    dc = DataCatalogue()
+    continuous_cols = dc.get_clustering_continuous()
+    categorical_cols = dc.get_clustering_categoricals()
+
+
     models = [GenerateKMEANS(), GenerateKprototypes(), GenerateHDBSCAN2()]
     for model in models:
         model_name = model.__class__.__name__
