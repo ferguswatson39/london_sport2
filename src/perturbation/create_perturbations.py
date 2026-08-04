4c417a99d85c53f010f2971446fa7bcc3a8dd29b
jn25200 <ferg.watson23@gmail.com>
2026-08-04 14:54:22 +0100

    refined create perturbations file and removed scaling from pipeline and only concider tree models

--- src/perturbation/create_perturbations.py
+++ src/perturbation/create_perturbations.py
@@ -43,120 +43,6 @@ simple_cases = {
     3 : {'model' : OLSRegressor(), 'target' : 'MEMS7_ALL',}
 }
 
-def create_perturbations(X_test : pd.DataFrame, Y_test : pd.DataFrame, model : object, continuous_vars : list[str]):
-    """
-    Perturbation Function.
-
-    Requires, df, model, scaler objects and specification of target var
-
-    Returns full df with original data and predictions in terms of probability for each var in to_perturb 
-
-    """
-    output_df = df.copy()
-    dc = DataCatalogue()
-    to_perturb = dc.get_perturbation_vars()
-    print(to_perturb)
-    print(df)
-    print(df.columns)
-    for var in to_perturb:
-        
-        new = df.copy()
-        if var not in new.columns:
-            print(f'{var} not in DataFrame pre perturbation')
-            continue
-        old_var_name = f'PREDS_{var}'
-        new_var_name = f'PERTURBED_PREDS_{var}'
-        difference = f'DIFFERENCE_{var}'
-
-        var_change = dc.get_perturbation_change(var)
-        var_max = dc.get_perturbation_max(var)
-        var_min = dc.get_perturbation_min(var)
-        var_values = new[var].values
-        var_idx = new.columns.get_loc(var)
-        
-        if var_change < 0:
-            mask = np.where(var_values > var_min, True, False)
-        elif var_change > 0:
-            mask = np.where(var_values < var_max, True, False)
-
-        X_masked = new.loc[mask].copy()
-
-        # X_scaled = scaler.transform(new[mask])
-        Y_test_masked = Y_test[mask]
-
-        # We only want to save the accuracy or mse metrics for the non perturbed set
-        # So use save_metric = True only for the first prediction generation
-        non_p_preds = model.get_preds(X_masked, Y_test_masked, save_metric=True)
-        X_masked[:, var_idx] = X_masked[:, var_idx] + var_change
-        p_preds = model.get_preds(X_masked, Y_test_masked, save_metric=False)
-
-        output_df.loc[mask, old_var_name] = non_p_preds
-        output_df.loc[mask, new_var_name] = p_preds
-        output_df.loc[mask, difference] = p_preds - non_p_preds
-    return output_df
-
-
-def create_and_join_diff_series(df : pd.DataFrame, group_by : str):
-    series = []
-    cols = [col for col in df.columns if 'DIFFERENCE' in col]
-    for c in cols:
-       # where group_by is either cluster 'labels' or borough code ect
-       x = df.groupby(group_by)[c].mean()
-       series.append(x)
-    return pd.concat(series, axis=1).reset_index()
-
-def build_perturbation_df(df : pd.DataFrame, model : object, target : str, group_by : str, cluster_col : str, continuous_vars : list[str], categoricals_to_encode : list[str]):
-    """
-    Purturbation Pipeline Function
-
-    Collects data from get_clean_2022 and implements one at a time sensitivity analysis following a 1 std change.
-
-    Returns the full perturbation dataset and also the grouped series of average changes by group_by.
-
-    For heatplot, use breakdowns.drop(columns='labels') then sns.heatmap....
-
-    if y != 'MEMS7_ALL' or y != 'active':
-        raise ValueError(f'{y} is not MEMS7_ALL or active')
-    clusters: ['LCA_Class'..... names of other cols whcih are attatched to clusters]
-    target: ['MEMS7_ALL', 'active']
-    """
-
-    print(f'These are the vars:\n{df.columns}')
-
-    if model.__class__.__name__ in ['OLSRegressor','Logistic']:
-        drop = 'first'
-    else:
-        drop = None
-    
-    df_encoded = one_hot_encode_frame(df, categoricals_to_encode, drop)
-
-    drop_cols = ['serial', 'year', cluster_col, 'MEMS7_ALL', 'active']
-    keep_vars = [var for var in df_encoded.columns if var not in drop_cols]
-    
-    labels = df_encoded[cluster_col]
-    Y = df_encoded[target]
-    X = df_encoded[keep_vars] # Keep vars contains all of the vars in the original df + encoded category names - the drop cols bit
-
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.4, random_state = 42, stratify = labels)
-    test_set_clusters = df_encoded.loc[X_test.index, cluster_col]
-
-    scaler = StandardScaler()
-
-    X_train[continuous_vars] = scaler.fit_transform(X_train[continuous_vars])
-
-    #X_train_scaled = scaler.fit_transform(X_train)
-    
-    model.fit(X_train, Y_train)
-    
-    perturbed_df = create_perturbations(X_test, model, scaler, Y_test, continuous_vars)
-
-    # Saves model to pkl file 
-    model.save_class()
-
-    perturbed_df['labels'] = test_set_clusters
-    breakdowns = create_and_join_diff_series(perturbed_df, group_by)
-    return perturbed_df, breakdowns
-
 def create_save_heatplot(breakdowns : pd.Series, target : str, heatplot_name : str):
     heatplot_path = ROOT / 'figures' / 'perturbation'
     if target == 'active':
@@ -177,7 +63,66 @@ def create_save_heatplot(breakdowns : pd.Series, target : str, heatplot_name : s
     plt.savefig(heatplot_path / heatplot_name)
     print(f'Saved hatplot figure to: {heatplot_path / heatplot_name}')
 
-def preprocess_perturbation(df : pd.DataFrame, model : object, cluster_column : str, target : str, continuous_vars : list[str], to_encode_vars : list[str]):
+def create_and_join_diff_series(df : pd.DataFrame, group_by : str):
+    series = []
+    cols = [col for col in df.columns if 'DIFFERENCE' in col]
+    for c in cols:
+       # where group_by is either cluster 'labels' or borough code ect
+       x = df.groupby(group_by)[c].mean()
+       series.append(x)
+    return pd.concat(series, axis=1).reset_index()
+
+def create_perturbations(X_test : pd.DataFrame, Y_test : pd.DataFrame, model : object, dc : object, to_perturb_vars : list[str]):
+    """
+    Perturbation Function.
+
+    Requires, X_test, Y_test, model, dataCatalogue objects and to_perturb var list
+
+    Returns full df with original data and predictions in terms of probability for each var in to_perturb 
+
+    """
+    output_df = X_test.copy()
+
+    for var in to_perturb_vars:
+        
+        new = X_test.copy()
+        if var not in new.columns:
+            print(f'{var} not in DataFrame pre perturbation')
+            continue
+
+        old_var_name = f'PREDS_{var}'
+        new_var_name = f'PERTURBED_PREDS_{var}'
+        difference = f'DIFFERENCE_{var}'
+
+        var_change = dc.get_perturbation_change(var)
+        var_max = dc.get_perturbation_max(var)
+        var_min = dc.get_perturbation_min(var)
+
+        var_values = new[var].values
+
+        
+        if var_change < 0:
+            mask = np.where(var_values > var_min, True, False)
+        elif var_change > 0:
+            mask = np.where(var_values < var_max, True, False)
+
+        X_masked = new.loc[mask].copy()
+
+        Y_test_masked = Y_test[mask]
+
+        # We only want to save the accuracy or mse metrics for the non perturbed set
+        # So use save_metric = True only for the first prediction generation
+        non_p_preds = model.get_preds(X_masked, Y_test_masked, save_metric=True)
+        X_masked[var] += var_change
+        p_preds = model.get_preds(X_masked, Y_test_masked, save_metric=False)
+
+        output_df.loc[mask, old_var_name] = non_p_preds
+        output_df.loc[mask, new_var_name] = p_preds
+        output_df.loc[mask, difference] = p_preds - non_p_preds
+    return output_df
+
+
+def preprocess_perturbation(df : pd.DataFrame, model : object, cluster_column : str, target : str, to_encode_vars : list[str]):
 
     if model.__class__.__name__ in ['OLSRegressor','Logistic']:
         drop = 'first'
@@ -202,21 +147,58 @@ def preprocess_perturbation(df : pd.DataFrame, model : object, cluster_column : 
 
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.4, random_state = 42, stratify = labels)
     test_set_clusters = df_encoded.loc[X_test.index, cluster_column]
-
-    scaler = StandardScaler()
-    X_train[continuous_vars] = scaler.fit_transform(X_train[continuous_vars])
-    X_test[continuous_vars] = scaler.transform(X_test[continuous_vars])
     
-    return X_train, X_test, Y_train, Y_test, test_set_clusters, scaler
+    return X_train, X_test, Y_train, Y_test, test_set_clusters
+
+
+if __name__ == '__main__':
+    dc = DataCatalogue()
+    df = get_clean_2022()
+    df_copy = df.copy()
+
+    to_perturb = dc.get_perturbation_vars()
+    continuous_vars = dc.get_perturbation_core_contins() + dc.get_perturbation_vars()
+    to_encode_vars = dc.get_perturbation_core_to_encode()
+
+    group_by = 'labels'
+    cluster_col = 'LCA_Class'
+    save_path = ROOT / 'results' / 'perturbation'
+
+    for key, value in run_cases.items():
+        model = run_cases[key]['model']
+        target = run_cases[key]['target']
+
+        X_train, X_test, Y_train, Y_test, test_set_clusters = preprocess_perturbation(df_copy, model, cluster_col, target, continuous_vars, to_encode_vars)
+
+        model.fit(X_train, Y_train)
+
+        perturbations = create_perturbations(X_test, Y_test, model, dc, to_perturb)
+
+        model.save_class()
+
+        perturbations[group_by] = test_set_clusters
+        breakdowns = create_and_join_diff_series(perturbations, group_by)
+
+        df_name = f'{key}_{model.__class__.__name__}_perturbation_results2.csv'
+        heatplot_name = f'{model.__class__.__name__}_perturbation_heatplot2.png'
+
+        perturbations.to_csv(save_path / df_name, index = False)
+        print(f'Saved pertubation results to: {save_path / df_name}')
+
+        create_save_heatplot(breakdowns, target, heatplot_name)
+        print(f'Finished perturbaiton for {model.__class__.__name__}')
+
+
+"""
 
 def execute_perturbation_pipeline(df : pd.DataFrame, run_cases : dict):
-    """
+
     Main perturbation function.
 
     Iterates through run cases, optimises and then trains models. 
 
     Saves perturbed dfs and perturbed heatplots
-    """
+
     save_path = ROOT / 'results' / 'perturbation'
 
     dc = DataCatalogue()
@@ -246,40 +228,56 @@ def execute_perturbation_pipeline(df : pd.DataFrame, run_cases : dict):
         print(f'Finished perturbaiton for {model.__class__.__name__}')
 
 
-if __name__ == '__main__':
-    dc = DataCatalogue()
-    df = get_clean_2022()
+def build_perturbation_df(df : pd.DataFrame, model : object, target : str, group_by : str, cluster_col : str, continuous_vars : list[str], categoricals_to_encode : list[str]):
 
-    df_copy = df.copy()
+    Purturbation Pipeline Function
 
-    continuous_vars = dc.get_perturbation_core_contins() + dc.get_perturbation_vars()
-    to_encode_vars = dc.get_perturbation_core_to_encode()
+    Collects data from get_clean_2022 and implements one at a time sensitivity analysis following a 1 std change.
 
-    group_by = 'labels'
-    cluster_col = 'LCA_Class'
+    Returns the full perturbation dataset and also the grouped series of average changes by group_by.
 
-    save_path = ROOT / 'results' / 'perturbation'
+    For heatplot, use breakdowns.drop(columns='labels') then sns.heatmap....
 
-    for key, value in run_cases.items():
-        model = run_cases[key]['model']
-        target = run_cases[key]['target']
+    if y != 'MEMS7_ALL' or y != 'active':
+        raise ValueError(f'{y} is not MEMS7_ALL or active')
+    clusters: ['LCA_Class'..... names of other cols whcih are attatched to clusters]
+    target: ['MEMS7_ALL', 'active']
 
-        X_train, X_test, Y_train, Y_test, test_set_clusters, scaler = preprocess_perturbation(df_copy, model, cluster_col, target)
 
-        model.fit(X_train, Y_train)
+    print(f'These are the vars:\n{df.columns}')
 
-        perturbations = create_perturbations(X_test, Y_test, model,continuous_vars, to_encode_vars)
+    if model.__class__.__name__ in ['OLSRegressor','Logistic']:
+        drop = 'first'
+    else:
+        drop = None
+    
+    df_encoded = one_hot_encode_frame(df, categoricals_to_encode, drop)
 
-        model.save_class()
+    drop_cols = ['serial', 'year', cluster_col, 'MEMS7_ALL', 'active']
+    keep_vars = [var for var in df_encoded.columns if var not in drop_cols]
+    
+    labels = df_encoded[cluster_col]
+    Y = df_encoded[target]
+    X = df_encoded[keep_vars] # Keep vars contains all of the vars in the original df + encoded category names - the drop cols bit
 
-        perturbations['labels'] = test_set_clusters
-        breakdowns = create_and_join_diff_series(perturbations, group_by)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.4, random_state = 42, stratify = labels)
+    test_set_clusters = df_encoded.loc[X_test.index, cluster_col]
 
-        df_name = f'{key}_{model.__class__.__name__}_perturbation_results2.csv'
-        heatplot_name = f'{model.__class__.__name__}_perturbation_heatplot2.png'
+    scaler = StandardScaler()
 
-        perturbations.to_csv(save_path / df_name, index = False)
-        print(f'Saved pertubation results to: {save_path / df_name}')
+    X_train[continuous_vars] = scaler.fit_transform(X_train[continuous_vars])
 
-        create_save_heatplot(breakdowns, target, heatplot_name)
-        print(f'Finished perturbaiton for {model.__class__.__name__}')
+    #X_train_scaled = scaler.fit_transform(X_train)
+    
+    model.fit(X_train, Y_train)
+    
+    perturbed_df = create_perturbations(X_test, Y_test, model, continuous_vars)
+
+    # Saves model to pkl file 
+    model.save_class()
+
+    perturbed_df['labels'] = test_set_clusters
+    breakdowns = create_and_join_diff_series(perturbed_df, group_by)
+    return perturbed_df, breakdowns
+
+"""
