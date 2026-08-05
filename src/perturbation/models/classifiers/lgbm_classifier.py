c69f6264060b44c6956124fb91d115a151695753
jn25200 <ferg.watson23@gmail.com>
2026-08-05 16:39:58 +0100

    debugged lgbmclassifier and rfclassifier classes and added accuracy eval metric

--- src/perturbation/models/classifiers/lgbm_classifier.py
+++ src/perturbation/models/classifiers/lgbm_classifier.py
@@ -1,6 +1,6 @@
 from lightgbm import LGBMClassifier, early_stopping, log_evaluation
 from sklearn.model_selection import cross_val_score
-from sklearn.metrics import f1_score, roc_auc_score
+from sklearn.metrics import f1_score, roc_auc_score, accuracy_score
 import optuna
 from pathlib import Path
 ROOT = Path(__file__).resolve().parent.parent.parent.parent.parent
@@ -15,23 +15,24 @@ class LightGBMClassifier:
         self.f1 = None
         self.roc_auc_score = None
         self.study = None
-        self.name = LightGBMClassifier.__name__
+        self.name = self.__class__.__name__
         self.save_path = ROOT / 'src' / 'perturbation' / 'models' / 'saved_models' 
 
     def objective(self, trial):
-        hyperparameters = {
-            'max_depth' : trial.suggest_int('max_depth', 3, 30),
-            'n_estimators' : trial.suggest_int('n_estimators', 100, 1000),
-            'learning_rate' : trial.suggest_float('learning_rate', 0.0001, 0.1, log=True),
-            'num_leaves' : trial.suggest_int('num_leaves', 5, 50),
-            'feature_fraction' : trial.suggest_float('feature_fraction', 0.5, 1.0),
-            'bagging_freq' : trial.suggest_int('bagging_freq', 1, 10),
-            'bagging_fraction' : trial.suggest_float('bagging_fraction', 0.5, 1.0),
-            'min_data_in_leaf' : trial.suggest_int('min_data_in_leaf', 10, 50)     
-        }
-        model = LGBMClassifier(**hyperparameters, random_state = 42, verbose=-1)
+        model = LGBMClassifier(
+            max_depth = trial.suggest_int('max_depth', 3, 30),
+            n_estimators = trial.suggest_int('n_estimators', 100, 1000),
+            learning_rate = trial.suggest_float('learning_rate', 0.0001, 0.1, log=True),
+            num_leaves = trial.suggest_int('num_leaves', 5, 50),
+            feature_fraction = trial.suggest_float('feature_fraction', 0.5, 1.0),
+            bagging_freq = trial.suggest_int('bagging_freq', 1, 10),
+            bagging_fraction = trial.suggest_float('bagging_fraction', 0.5, 1.0),
+            min_data_in_leaf= trial.suggest_int('min_data_in_leaf', 10, 50),
+            random_state = 42,
+            verbose = -1
+        )
         # Scoring here is roc_auc but maybe i should try_ f1
-        cv_score = cross_val_score(model, self.X_train, self.Y_train, cv=5, scoring = 'roc_auc')
+        cv_score = cross_val_score(model, self.X_train, self.Y_train, cv=5, scoring = 'macro_f1')
         return cv_score.mean()
     
     def run_study(self):
@@ -44,18 +45,20 @@ class LightGBMClassifier:
     
     def fit(self, X_train, Y_train):
         self.X_train, self.Y_train = X_train, Y_train
-        print(f'Starting to run study....')
+        print(f'Starting to run study for {self.name}....')
         self.study = self.run_study()
-        print(f'Finished running study. Optimal hyperparameters found.')
-        print(f'Fitting {LightGBMClassifier.__name__}...')
+        print(f'Optimal hyperparameters found for {self.name}.')
+        print(f'Fitting {self.name}...')
         self.model.fit(X_train, Y_train)
 
     def get_preds(self, X_test, Y_test, save_metric : bool):
-        preds_prob = self.model.predict_proba(X_test)[:, 1]
+        preds_prob = self.model.predict_proba(X_test)
         preds_class = self.model.predict(X_test)
         if save_metric:
-            self.f1 = f1_score(Y_test, preds_class)
-            self.roc_auc = roc_auc_score(Y_test, preds_prob)
+            self.f1 = f1_score(Y_test, preds_class, average='macro')
+            # Use 'ovo' to adjust for class imbalances
+            self.roc_auc = roc_auc_score(Y_test, preds_prob, multi_class='ovo', average='macro')
+            self.accuracy = accuracy_score(Y_test, preds_class)
         return preds_prob
 
     def get_model(self):
@@ -67,9 +70,9 @@ class LightGBMClassifier:
         return self.f1
     
     def save_class(self):
-        filename = f'{self.__class__.__name__}.sav'
+        filename = f'{self.name}.sav'
         path = ROOT / 'src' / 'perturbation' / 'models' / 'saved_models' / filename
         with open(path, 'wb') as file:
             pickle.dump(self, file)
-        print(f'{self.__class__.__name__} saved to: {path}')
+        print(f'{self.name} saved to: {path}')
 
--- src/perturbation/models/classifiers/rf_classifier.py
+++ src/perturbation/models/classifiers/rf_classifier.py
@@ -3,7 +3,7 @@ from sklearn.metrics import classification_report
 import pandas as pd
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import cross_val_score
-from sklearn.metrics import f1_score, roc_auc_score
+from sklearn.metrics import f1_score, roc_auc_score, accuracy_score
 import optuna
 from pathlib import Path
 ROOT = Path(__file__).resolve().parent.parent.parent.parent.parent
@@ -21,19 +21,20 @@ class RFClassifier:
         self.Y_train = None
         self.f1 = None
         self.roc_auc_score = None
+        self.accuracy = None
         self.study = None
-        self.name = RFClassifier.__name__
+        self.name = self.__class__.__name__
         self.save_path = ROOT / 'src' / 'perturbation' / 'models' / 'saved_models' 
 
     def objective(self, trial):
-        hyperparameters = {
-            'max_depth' : trial.suggest_int('max_depth', 3, 30),
-            'n_estimators' : trial.suggest_int('n_estimators', 100, 1000),
-            'max_features' : trial.suggest_categorical('max_features', ['sqrt', 'log2', 1.0]),
-            'min_samples_leaf' : trial.suggest_int('min_samples_leaf', 1, 20)            
-        }
-        model = RandomForestClassifier(**hyperparameters, random_state = 42)
-        cv_score = cross_val_score(model, self.X_train, self.Y_train, cv=5, scoring = 'roc_auc')
+        model = RandomForestClassifier(
+            max_depth = trial.suggest_int('max_depth', 3, 30),
+            n_estimators = trial.suggest_int('n_estimators', 100, 1000),
+            max_features = trial.suggest_categorical('max_features', ['sqrt', 'log2', 1.0]),
+            min_samples_leaf = trial.suggest_int('min_samples_leaf', 1, 20),
+            random_state = 42  
+            )
+        cv_score = cross_val_score(model, self.X_train, self.Y_train, cv=5, scoring = 'f1_macro')
         return cv_score.mean()
     
     def run_study(self):
@@ -45,19 +46,21 @@ class RFClassifier:
     
     def fit(self, X_train, Y_train):
         self.X_train, self.Y_train = X_train, Y_train
-        print(f'Starting to run study....')
+        print(f'Starting to run study for {self.name}....')
         self.study = self.run_study()
-        print(f'Finished running study. Optimal hyperparameters found.')
-        print(f'Fitting {RFClassifier.__name__}...')
+        print(f'Optimal hyperparameters found for {self.name}.')
+        print(f'Fitting {self.name}...')
         self.model.fit(X_train, Y_train)
 
     
     def get_preds(self, X_test, Y_test, save_metric : bool):
-        preds_prob = self.model.predict_proba(X_test)[:, 1]
+        preds_prob = self.model.predict_proba(X_test)
         preds_class = self.model.predict(X_test)
         if save_metric:
-            self.f1 = f1_score(Y_test, preds_class)
-            self.roc_auc = roc_auc_score(Y_test, preds_prob)
+            self.f1 = f1_score(Y_test, preds_class, average='macro')
+            # Use 'ovo' to adjust for class imbalances
+            self.roc_auc = roc_auc_score(Y_test, preds_prob, multi_class='ovo', average='macro')
+            self.accuracy = accuracy_score(Y_test, preds_class)
         return preds_prob
     
     def get_model(self):
@@ -69,9 +72,9 @@ class RFClassifier:
         return self.f1
     
     def save_class(self):
-        filename = f'{self.__class__.__name__}.sav'
+        filename = f'{self.name}.sav'
         path = ROOT / 'src' / 'perturbation' / 'models' / 'saved_models' / filename
         with open(path, 'wb') as file:
             pickle.dump(self, file)
-        print(f'{self.__class__.__name__} saved to: {path}')
+        print(f'{self.name} saved to: {path}')
     
