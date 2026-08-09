8e2930c4f28bdde451b466e15e78f58b38d9889f
jn25200 <ferg.watson23@gmail.com>
2026-08-09 13:19:26 +0100

    added function to perturbation pipeline to save count of perturbed samples by cluster by variable

--- src/perturbation/create_perturbations.py
+++ src/perturbation/create_perturbations.py
@@ -53,6 +53,7 @@ def create_perturbations(X_test : pd.DataFrame, Y_test : pd.DataFrame, model : o
 
     continuous_vars = dc.get_perturbation_core_contins() + dc.get_perturbation_vars()
 
+    perturbable_samples = []
     for var in to_perturb_vars:
         
         df = X_test.copy()
@@ -66,11 +67,13 @@ def create_perturbations(X_test : pd.DataFrame, Y_test : pd.DataFrame, model : o
 
         var_values = df[var].values
 
-        
+
         if var_change < 0:
             mask = np.where(var_values > var_min, True, False)
         elif var_change > 0:
             mask = np.where(var_values < var_max, True, False)
+
+        perturbable_samples.append((var, mask.sum()))
 
         X_masked = df.loc[mask].copy()
         X_masked_scaled = X_masked.copy()
@@ -95,7 +98,45 @@ def create_perturbations(X_test : pd.DataFrame, Y_test : pd.DataFrame, model : o
         output_df.loc[mask, perturbed_predictions] = p_preds
         output_df.loc[mask, difference] = p_preds - non_p_preds
 
+    for var, num in perturbable_samples:
+        print(f'{var} has {num} perturbable samples')
+    
     return output_df
+
+def check_perturbed_counts(dc = DataCatalogue()):
+
+    df_path = ROOT / 'results' / 'perturbation' / '103_LightGBMClassifier_perturbation_results_FINAL.csv'
+    df = pd.read_csv(df_path)
+
+    save_path = ROOT / 'results' / 'perturbation'
+    file_name = 'perturbation_counts.csv'
+
+    diff_cols = [var for var in df.columns if 'DIFFERENCE' in var]
+    results = []
+    
+    for var in diff_cols:
+        stem = var.replace('DIFFERENCE_', '')
+
+        var_change = dc.get_perturbation_change(stem)
+        var_max = dc.get_perturbation_max(stem)
+        var_min = dc.get_perturbation_min(stem)
+        var_values = df[stem].values
+
+        if var_change < 0:
+            mask = np.where(var_values > var_min, True, False)
+        elif var_change > 0:
+            mask = np.where(var_values < var_max, True, False)
+
+        counts = df[mask].groupby('labels')[var].count()
+
+        results.append(counts)
+
+    df = pd.concat(results, axis=1)
+
+    df.to_csv(save_path / file_name, index=False)
+
+    print(f'Saved {file_name} to {save_path}')
+
 
 if __name__ == '__main__':
     # Can edit run cases here to change run cases
@@ -130,161 +171,8 @@ if __name__ == '__main__':
     print(f'Saved pertubation results to: {save_path / df_name}')
 
     create_save_heatplot(breakdowns, target, heatplot_name)
-    print(f'Finished perturbaiton for {model.__class__.__name__}')
 
+    print('Checking perturbation counts...')
+    check_perturbed_counts()
 
-
-
-
-"""
-
-def execute_perturbation_pipeline(df : pd.DataFrame, run_cases : dict):
-
-    Main perturbation function.
-
-    Iterates through run cases, optimises and then trains models. 
-
-    Saves perturbed dfs and perturbed heatplots
-
-    save_path = ROOT / 'results' / 'perturbation'
-
-    dc = DataCatalogue()
-    continuous_vars = dc.get_perturbation_core_contins()
-    categoricals_to_encode = dc.get_perturbation_core_to_encode()# ['Gend3', 'Eth7', 'WorkStat8', 'HHLiv9'] doesnt include disab2
-
-    print(f'Perturbation core continuous vars:\n>>> {continuous_vars}')
-
-    for key, value in run_cases.items():
-  
-        model = run_cases[key]['model']
-        target = run_cases[key]['target']
-
-        group_by = 'labels'
-        cluster_col = 'LCA_Class'
-
-        df_name = f'{key}_{model.__class__.__name__}_perturbation_results2.csv'
-        heatplot_name = f'{model.__class__.__name__}_perturbation_heatplot2.png'
-
-        print(f'Starting Perturbation for {model.__class__.__name__}')
-        perturbed_df, breakdowns = build_perturbation_df(df, model, target, group_by, cluster_col, continuous_vars, categoricals_to_encode)
-
-        perturbed_df.to_csv(save_path / df_name, index = False)
-        print(f'Saved pertubation results to: {save_path / df_name}')
-
-        create_save_heatplot(breakdowns, target, heatplot_name)
-        print(f'Finished perturbaiton for {model.__class__.__name__}')
-
-
-def build_perturbation_df(df : pd.DataFrame, model : object, target : str, group_by : str, cluster_col : str, continuous_vars : list[str], categoricals_to_encode : list[str]):
-
-    Purturbation Pipeline Function
-
-    Collects data from get_clean_2022 and implements one at a time sensitivity analysis following a 1 std change.
-
-    Returns the full perturbation dataset and also the grouped series of average changes by group_by.
-
-    For heatplot, use breakdowns.drop(columns='labels') then sns.heatmap....
-
-    if y != 'MEMS7_ALL' or y != 'active':
-        raise ValueError(f'{y} is not MEMS7_ALL or active')
-    clusters: ['LCA_Class'..... names of other cols whcih are attatched to clusters]
-    target: ['MEMS7_ALL', 'active']
-
-
-    print(f'These are the vars:\n{df.columns}')
-
-    if model.__class__.__name__ in ['OLSRegressor','Logistic']:
-        drop = 'first'
-    else:
-        drop = None
-    
-    df_encoded = one_hot_encode_frame(df, categoricals_to_encode, drop)
-
-    drop_cols = ['serial', 'year', cluster_col, 'MEMS7_ALL', 'active']
-    keep_vars = [var for var in df_encoded.columns if var not in drop_cols]
-    
-    labels = df_encoded[cluster_col]
-    Y = df_encoded[target]
-    X = df_encoded[keep_vars] # Keep vars contains all of the vars in the original df + encoded category names - the drop cols bit
-
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.4, random_state = 42, stratify = labels)
-    test_set_clusters = df_encoded.loc[X_test.index, cluster_col]
-
-    scaler = StandardScaler()
-
-    X_train[continuous_vars] = scaler.fit_transform(X_train[continuous_vars])
-
-    #X_train_scaled = scaler.fit_transform(X_train)
-    
-    model.fit(X_train, Y_train)
-    
-    perturbed_df = create_perturbations(X_test, Y_test, model, continuous_vars)
-
-    # Saves model to pkl file 
-    model.save_class()
-
-    perturbed_df['labels'] = test_set_clusters
-    breakdowns = create_and_join_diff_series(perturbed_df, group_by)
-    return perturbed_df, breakdowns
-
-    
-
-
-
-
-
-
-if __name__ == '__main__':
-    # Can edit run cases here to change run cases
-    run_cases = classification_cases
-    target = 'active_status'
-
-    dc = DataCatalogue()
-    df = get_clean_2022()
-    df_copy = df.copy()
-
-    to_perturb = dc.get_perturbation_vars()
-    continuous_vars = dc.get_perturbation_core_contins() + dc.get_perturbation_vars()
-    to_encode_vars = dc.get_perturbation_core_to_encode()
-
-    group_by = 'labels'
-    cluster_col = 'LCA_Class'
-    save_path = ROOT / 'results' / 'perturbation'
-
-    X_train, X_test, Y_train, Y_test, test_set_clusters = preprocess_perturbation(df_copy, cluster_col, target, to_encode_vars)
-
-    for key, value in run_cases.items():
-
-        run_num = key
-        model = run_cases[key]['model']
-        do_smote = run_cases[key]['smote']
-        k_neigh = run_cases[key]['k_neighbors']
-        strat = run_cases[key]['strategy']
-        # target = run_cases[key]['target']
-
-        if do_smote:
-            print(f'Resampling with smote for run number: {run_num}')
-            X_train, Y_train = smote_resample(X_train, Y_train, k_neigh, strat)
-    
-        scaler = StandardScaler()
-        X_train_scaled = scaler.fit_transform(X_train)
-        model.fit(X_train_scaled, Y_train)
-
-
-        perturbations = create_perturbations(X_test, Y_test, model, scaler, to_perturb)
-
-        model.save_class()
-
-        perturbations[group_by] = test_set_clusters
-        #breakdowns = create_and_join_diff_series(perturbations, group_by)
-
-        df_name = f'{key}_{model.__class__.__name__}_perturbation_results3.csv'
-        # heatplot_name = f'{model.__class__.__name__}_perturbation_heatplot2.png'
-
-        perturbations.to_csv(save_path / df_name, index = False)
-        print(f'Saved pertubation results to: {save_path / df_name}')
-
-        # create_save_heatplot(breakdowns, target, heatplot_name)
-        print(f'Finished perturbaiton for {model.__class__.__name__}')
-
-"""
+    print(f'Finished perturbaiton process for {model.__class__.__name__}')
