3b1e5cad7cd49952de306bd905cbc3d288373711
Matt Hall <matt_matthall@outlook.com>
2026-09-03 15:53:46 +0100

    Introduce sensitivity analysis to determine subgroups underrepresented due to the exclusion of missing data

---
+++ exploration/forecasting_eda/clustering_variable_label_dictionary.py
@@ -0,0 +1,140 @@
+value_labels = {
+
+    "Age9": {
+        0: "16–24",
+        1: "25–34",
+        2: "35–44",
+        3: "45–54",
+        4: "55–64",
+        5: "65–74",
+        6: "75–84",
+        7: "85+"
+    },
+
+    "Gend3": {
+        0: "Male",
+        1: "Female",
+        2: "Other"
+    },
+
+    "Eth7": {
+        0: "White British",
+        1: "White Other",
+        2: "Asian (excl. Chinese)",
+        3: "Black",
+        4: "Chinese",
+        5: "Mixed",
+        6: "Other ethnic group"
+    },
+
+    "Disab2_POP": {
+        0: "Disability",
+        1: "No disability"
+    },
+
+    "Educ6": {
+        0: "Level 4 or above",
+        1: "Level 3 and equivalents",
+        2: "Level 2 and equivalents",
+        3: "Level 1 and below",
+        4: "Another type of qualification",
+        5: "No qualifications"
+    },
+
+    "NSSEC5": {
+        0: "NS SEC 1–2: Higher social groups",
+        1: "NS SEC 3–5: Middle social groups",
+        2: "NS SEC 6–8: Lower social groups",
+        3: "NS SEC 9: Students and other / unclassified",
+        4: "Not applicable (75+)"
+    },
+
+    "IMD10": {
+        0: "Most deprived decile",
+        1: "Second most deprived decile",
+        2: "Third most deprived decile",
+        3: "Fourth most deprived decile",
+        4: "Fifth most deprived decile",
+        5: "Fifth least deprived decile",
+        6: "Fourth least deprived decile",
+        7: "Third least deprived decile",
+        8: "Second least deprived decile",
+        9: "Least deprived decile"
+    },
+
+    "WorkStat8": {
+        0: "Working full time",
+        1: "Working part time",
+        2: "Unemployed",
+        3: "Not working-retired",
+        4: "Not working-looking after house/children",
+        5: "Not working-long term sick or disabled",
+        6: "Student",
+        7: "Other"
+    },
+
+    "Child4": {
+        0: "None",
+        1: "1 child",
+        2: "2 children",
+        3: "3 or more children"
+    },
+
+    "HHLiv9": {
+        0: "Single person living alone",
+        1: "Houseshare",
+        2: "Lone parent family",
+        3: "Couple, no children",
+        4: "Couple with children",
+        5: "Couple with adult children",
+        6: "Multi-generational household",
+        7: "Living with parents",
+        8: "Other/complex household"
+    },
+
+    "Motiva_POP": {
+        0: "Strongly agree",
+        1: "Agree",
+        2: "Neither agree nor disagree",
+        3: "Disagree",
+        4: "Strongly disagree"
+    },
+
+    "motivd_POP": {
+        0: "Strongly agree",
+        1: "Agree",
+        2: "Neither agree nor disagree",
+        3: "Disagree",
+        4: "Strongly disagree"
+    }
+
+}
+
+display_names = {
+    "Age9": "Age",
+
+    "Gend3": "Gender",
+
+    "Eth7": "Ethnicity",
+
+    "Disab2_POP": "Disability",
+
+    "Educ6": "Education",
+
+    "NSSEC5": "Socio-economic Status",
+
+    "IMD10": "Deprivation",
+
+    "WorkStat8": "Employment Status",
+
+    "Child4": "Children",
+
+    "HHLiv9": "Household Composition",
+
+    "VolAny": "Volunteering",
+
+    "Motiva_POP": "Intrinsic Motivation",
+
+    "motivd_POP": "Extrinsic Motivation"
+
+}
